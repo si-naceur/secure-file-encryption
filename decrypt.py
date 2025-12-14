c815c927f44bb7e6933fca9aac476e46f5ce1c6f
si-naceur <naceu.zidi@gmail.com>
2025-12-14 21:47:59 +0100

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,17 +1,14 @@
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
-# فتح private key الموجود عندك
 private_key = RSA.import_key(open("keys/private.pem").read())
 cipher_rsa = PKCS1_OAEP.new(private_key)
 
-# فتح AES key المشفّر
 with open("keys/session_key.enc", "rb") as f:
     enc_session_key = f.read()
 
 session_key = cipher_rsa.decrypt(enc_session_key)
 
-# فتح الملف المشفّر
 with open("sample.txt.enc", "rb") as f:
     nonce = f.read(16)
     tag = f.read(16)
@@ -20,7 +17,6 @@ with open("sample.txt.enc", "rb") as f:
 cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce=nonce)
 data = cipher_aes.decrypt_and_verify(ciphertext, tag)
 
-# حفظ الملف بعد فك التشفير
 with open("sample_decrypted.txt", "wb") as f:
     f.write(data)
 
