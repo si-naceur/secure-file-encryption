f1d014ca1ec6a67194c9f2c17ad1d6efe691a0f6
si-naceur <naceu.zidi@gmail.com>
2025-12-14 21:47:29 +0100

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -1,13 +1,10 @@
 from Crypto.PublicKey import RSA
 
-# إنشاء مفتاح جديد 2048 bit
 key = RSA.generate(2048)
 
-# حفظ المفتاح العمومي
 with open("keys/public.pem", "wb") as f:
     f.write(key.publickey().export_key())
 
-# حفظ المفتاح السري
 with open("keys/private.pem", "wb") as f:
     f.write(key.export_key())
 
